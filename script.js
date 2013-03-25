035ee366f24bbfcfd45c94bd20f8920de09f1c43
vy4eslavik <vy4eslavik@yandex.ua>
2013-03-25 21:30:55 +0200

    Наброски. Разметка, стили, скрипт загрузки фото с сервера

---
+++ script.js
@@ -0,0 +1,42 @@
+//general begin
+var downloadSetting = {
+    domen: 'http://api-fotki.yandex.ru',
+    user: 'aig1001',
+    albumId: '63684',
+    param: '&format=json&callback=?'
+};
+var images = [];
+
+var loading = $('<div/>', {
+    class: 'b_gallery-b_thumb-e_loading'
+});
+//general end
+
+function downloadImg() {
+    $('.b_gallery-b_thumb').append(loading);
+    var url = downloadSetting.domen + "/api/users/"+downloadSetting.user+"/album/"+downloadSetting.albumId+"/photos/?"+downloadSetting.param;
+    var dfd = $.Deferred();
+    $.getJSON(url,function(data){
+        for (var i in data.entries)
+        {
+            if(i>10) {break;}
+            var reg = /(\d+)$/g;
+            var id = reg.exec(data.entries[i].id);
+            var imgObj = {
+                thumb : data.entries[i].img.S.href,
+                fullImg : data.entries[i].img.L.href,
+                width : data.entries[i].img.L.width,
+                height : data.entries[i].img.L.height,
+                id: id[1]
+            }
+            images.push(imgObj);
+            dfd.resolve();
+        }
+
+        dfd.done(test());
+    });
+}
+downloadImg();
+function test() {
+    alert(images[10].fullImg);
+}
